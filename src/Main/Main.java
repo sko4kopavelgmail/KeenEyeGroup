da23b0101cb852c5222e6e8b165a0651a6b4fe01
qckl <sobakogavgav@gmail.com>
2019-12-06 19:41:21 +0300

    f

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,8 @@
 package Main;
 
-public class Main {
+ public class Main {
 
-    public static void main(String[] args) {
+    public static void main( String[] args) {
 
     }
 
--- src/Utils/DateUtils.java
+++ src/Utils/DateUtils.java
@@ -1,4 +1,4 @@
-package Utils;
+ package Utils;
 
 public class DateUtils {
 }
