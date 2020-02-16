9f0ff5c6edcd1667efa59467629a1b84750d2ae4
Ersis404 <mihaf007700@ya.ru>
2020-02-16 12:36:44 +0300

    Все условия выполнены, есть возможность добавления бесчисленного количесва новых команд, для этого Редактируем Enum и добавояем в Manager функцию команды,а так же добавляем case команды.

--- src/Enum/Lessons.java
+++ src/Enum/Lessons.java
@@ -1,4 +1,25 @@
 package Enum;
 
 public enum Lessons {
+    help("help", "?", "Команда 'help' или '?' отображает список команд, а так же их назначение!"),
+    exit("exit","ex", "Команда 'exit' или 'ex' закрывает консоль!");
+
+    String Full_name,name,info;
+    Lessons (String Full_name, String name, String info){
+        this.Full_name=Full_name;
+        this.name=name;
+        this.info=info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFull_name() {
+        return Full_name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
 }
