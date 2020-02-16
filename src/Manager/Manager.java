003cdf9804a7deedb941d51b632ff57f74ed442b
Ersis404 <mihaf007700@ya.ru>
2020-02-16 12:49:30 +0300

    1

--- src/Manager/Manager.java
+++ src/Manager/Manager.java
@@ -1,6 +1,33 @@
 package Manager;
 
+import Enum.Lessons;
 public class Manager {
 
+    public static void execute(String command) {
+        try {
+            Lessons cname = Lessons.valueOf(command);
+
+            switch (cname) {
+                case help:
+                    help ();
+                    break;
+                case exit:
+                    exit();
+                    break;
+            }
+        } catch (IllegalArgumentException iae) {
+            // Unknown command
+        }
+    }
+
+    private static void help(){
+        Lessons[] data = Lessons.values();
+        for(int i = 0; i<data.length;i++)
+            System.out.println(data[i].getInfo());
+    }
+
+    private static void exit(){
+        System.exit(0);
+    }
 
 }
